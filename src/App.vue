8719da50ce320fae132f8ef16b7abf81d60a4af4
Aleksey Yuriev <alex11-90@yandex.by>
2024-08-16 18:49:11 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <AppHeader />
+    <h1>Hello!</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppHeader from "@/components/AppHeader.vue"
+</script>
+
+<style scoped></style>
