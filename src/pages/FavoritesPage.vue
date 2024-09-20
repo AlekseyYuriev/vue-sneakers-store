101bcfdc57e4ceb332a21e1f45105eccfd1226b1
Aleksey Yuriev <alex11-90@yandex.by>
2024-09-20 18:52:10 +0300

    feat: added home button to FavoritesPage

--- src/pages/FavoritesPage.vue
+++ src/pages/FavoritesPage.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-    <h2 class="text-3xl font-bold mb-20">Мои закладки</h2>
+    <div class="flex gap-5">
+      <RouterLink to="/">
+        <div
+          class="h-9 w-9 border border-slate-200 rounded-xl flex items-center justify-center cursor-pointer hover:border-orange-300 transition hover:scale-105"
+        >
+          <svg
+            width="7"
+            height="12"
+            viewBox="0 0 7 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6 11L1 6L6 1"
+              stroke="#C8C8C8"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+      </RouterLink>
+      <h2 class="text-3xl font-bold mb-8">Мои закладки</h2>
+    </div>
 
     <AppCardList
       :sneakers="favorites"
