79f7a6f212b73df9704ab54f83fc1dda05b902a2
Aleksey Yuriev <alex11-90@yandex.by>
2026-05-02 17:00:15 +0300

    fix: update AppCard component styles forproduct details and toggle actions

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,31 +1,30 @@
 <template>
-  <div>
-    <div
-      class="relative flex flex-col w-full border border-slate-100 rounded-xl p-8 cursor-pointer transition hover:shadow-xl hover:transform hover:-translate-y-2"
+  <div
+    class="relative flex flex-col w-full h-full border border-slate-100 rounded-xl p-8 cursor-pointer transition hover:shadow-xl hover:transform hover:-translate-y-2"
+  >
+    <button
+      @click="handleFavorite"
+      class="absolute top-8 left-8 disabled:cursor-wait"
+      :disabled="isLoading"
     >
-      <button
-        @click="handleFavorite"
-        class="absolute top-8 left-8 disabled:cursor-wait"
-        :disabled="isLoading"
-      >
-        <img :src="isFavorite ? '/like-2.svg' : '/like-1.svg'" alt="Favorite" />
-      </button>
-      <img class="w-full" :src="imageUrl" alt="Sneaker" />
-      <p class="mt-2 h-12">{{ title }}</p>
+      <img :src="isFavorite ? '/like-2.svg' : '/like-1.svg'" alt="Favorite" />
+    </button>
+    <img class="w-full" :src="imageUrl" alt="Sneaker" />
+    <p class="mt-2">{{ title }}</p>
 
-      <div class="flex justify-between mt-5">
-        <div class="flex flex-col">
-          <span class="text-slate-400">Цена:</span>
-          <b>{{ price }} руб.</b>
-        </div>
-
-        <img
-          v-if="isAdded !== undefined"
-          @click="handleCart"
-          :src="isAdded ? '/checked.svg' : '/plus.svg'"
-          alt="Plus"
-        />
+    <div class="flex justify-between mt-auto pt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>{{ price }} руб.</b>
       </div>
+
+      <img
+        v-if="isAdded !== undefined"
+        @click="handleCart"
+        class="w-8 h-8"
+        :src="isAdded ? '/checked.svg' : '/plus.svg'"
+        alt="Plus"
+      />
     </div>
   </div>
 </template>
